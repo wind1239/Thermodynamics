91e90c515c0b08b6ba196d2f918651e66d201e56
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2015-03-23 14:19:53 +0000

    updates

--- PythonUG_Models/Optimiser_OMOF/Fugacity.py
+++ PythonUG_Models/Optimiser_OMOF/Fugacity.py
@@ -14,6 +14,7 @@ import MixingRule_Simple as MixS
 ### FUNCTION: Calculate fugacity of components
 ###
 def CalcFugacity( T, P, MoleFraction, phase ):
+    
     ThT.set_Global_Variables()
 
     fugacity = [ 0. for i in range( ThT.Nc ) ]
@@ -41,12 +42,12 @@ def CalcFugacity( T, P, MoleFraction, phase ):
                 aij[ node ] = PR.PREoS_Calc_a( i , T )
             else:
                 aij[ node ] = math.sqrt( PR.PREoS_Calc_a( i , T ) * PR.PREoS_Calc_a( j , T ) ) * ( 1. - ThT.kij[ node ] )
-
             sum1 = sum1 + aij[ node ] * MoleFraction[ j ]
+            
         fugacity[ i ] = ( PR.PREoS_Calc_b( i ) / bm * ( Z - 1. ) - math.log( Z - Big_B ) - Big_A / ( 2. * math.sqrt( 2. ) * Big_B ) *
                        ( 2. * sum1 / am - PR.PREoS_Calc_b( i ) / bm  ) * math.log( ( Z + ( 1. - math.sqrt( 2. ) ) * Big_B ) /
                                                                                    ( Z + ( 1. + math.sqrt( 2. ) ) * Big_B ) )  )
-        fugacity[ i ] = MoleFraction[ i ] * P * fugacity[ i ]
+        fugacity[ i ] = MoleFraction[ i ] * P * math.exp( fugacity[ i ] )
 
     return fugacity
         
