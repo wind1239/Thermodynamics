60ae3c427eef86b9f41b3f58dbfc13d04c10678c
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2016-01-25 10:09:46 +0000

    Updates: Finished loop, add a function that ensures unity of a normalised array (mass/mole fraction)

--- VLE_Kostas/ThermoTools.py
+++ VLE_Kostas/ThermoTools.py
@@ -17,7 +17,7 @@ import sys
 def ReadSet_Global_Variables(): # Read variables from a external file called 'input.dat'
     import csv # Using csv (comma separated values) module
  
-    global Rconst, Ncomp, T_System, P_System, T_Crit, P_Crit, MolarMass, Species, Accentric_Factor, \
+    global Rconst, NComp, NPhase, T_System, P_System, T_Crit, P_Crit, MolarMass, Species, Accentric_Factor, \
         Z_Feed, BinaryParameter
 
     Rconst = 8.314 # Gas constant [J/(gmol.K)]
@@ -37,11 +37,14 @@ def ReadSet_Global_Variables(): # Read variables from a external file called 'in
                 Nothing_To_Be_Done = True
 
             elif row[ 0 ] == 'Number_Components': # This MUST be the first variable declared in the input file
-                Ncomp = int( row[ 1 ] )
+                NComp = int( row[ 1 ] )
                 Are_There_Components = True
 
             elif Are_There_Components:
-                if row[ 0 ] == 'System_Temperature': # Temperature of the mixture (in K)
+                if row[ 0 ] == 'Number_Phases': # Number of phases
+                    NPhase = int( row[ 1 ] )
+    #
+                elif row[ 0 ] == 'System_Temperature': # Temperature of the mixture (in K)
                     T_System = Reading_MixtureConditions_Float( row ) 
     #
                 elif row[ 0 ] == 'System_Pressure': # Pressure of the mixture (in bar)
@@ -79,8 +82,8 @@ def ReadSet_Global_Variables(): # Read variables from a external file called 'in
 
 # This function reads a row containing float elements
 def ReadingRows_Float( row ):
-    Array = np.arange( float( Ncomp ) )
-    for i in xrange( 0, Ncomp ):
+    Array = np.arange( float( NComp ) )
+    for i in xrange( 0, NComp ):
         Array[ i ] = row[ i + 1 ]
     return Array
 
@@ -105,13 +108,13 @@ def Reading_MixtureConditions_Float( row ):
 # This function reads a row containing characters elements
 def ReadingRows_String( row ):
     Array = []
-    Array.extend( row[ 1 : Ncomp + 1 ] )
+    Array.extend( row[ 1 : NComp + 1 ] )
     
     return Array
 
 # This function populates the binary interaction parameter (Kij) tensor in which Kij = Kji 
-#    and Kij = 0 (for i=1,Ncomp and j=1,Ncomp). Kij is stored as an array Kij[ node ] with
-#                             node = i * Ncomp + j
+#    and Kij = 0 (for i=1,NComp and j=1,NComp). Kij is stored as an array Kij[ node ] with
+#                             node = i * NComp + j
 #    For 3 components:
 #           K11   K12   K13
 #           K21   K22   K23
@@ -120,23 +123,23 @@ def ReadingRows_String( row ):
 #                  K12 = K21, K13 = K31 and K23 = K32
 #
 def ReadingBinaryParameters( reader ):
-    Kij = [ 0. for i in range( Ncomp ** 2 ) ]
+    Kij = [ 0. for i in range( NComp ** 2 ) ]
     Array_temp = [] 
 
     for line in reader: # Reading input file for Kij and allocating the data in a list
         Array_temp.append(line)
 
-    for k in range( NumberOfCombinations( Ncomp, 2 ) ):
+    for k in range( NumberOfCombinations( NComp, 2 ) ):
         temp = Array_temp[ k ]
         i = int( temp[ 0 ] ) ; j = int( temp[ 1 ] )
-        node = ( i - 1 ) * Ncomp + ( j - 1 )
+        node = ( i - 1 ) * NComp + ( j - 1 )
         Kij[ node ] = float( temp[ 2 ] ) # Populating the upper triangular region
 
-    for i in range( Ncomp ):
-        for j in range( Ncomp ):
-            node1 = i * Ncomp + j
+    for i in range( NComp ):
+        for j in range( NComp ):
+            node1 = i * NComp + j
             if i > j:
-                node2 = j * Ncomp + i 
+                node2 = j * NComp + i 
                 Kij[ node1 ] = Kij[ node2 ] # Now populating the lower triangular region
 
     return Kij
@@ -158,7 +161,6 @@ def ListOfCommentsStrings( row ):
              row[ 0 ] == ' '     or \
              row[ 0 ] == '  '    or \
              row[ 0 ] == '   '   or \
-             row[ 0 ] == ''      or \
              row[ 0 ] == '#'     or \
              row[ 0 ] == '##'    or \
              row[ 0 ] == '###'   or \
@@ -173,3 +175,11 @@ def ListOfCommentsStrings( row ):
     return IsItaComment
     
 
+# This function checks if an normalised compositional array (e.g., mass, mole or volume fractions)
+#     sums up to one
+def Sum2One( ident, Array ):
+    if abs( math.fsum( Array ) - 1. ) >= 1.e-7:
+        print 'Array ', ident, ' is not normalised correctly:', math.fsum( Array )
+        sys.exit()
+
+    return
