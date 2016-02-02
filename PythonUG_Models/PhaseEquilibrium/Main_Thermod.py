31c63ba5a4692d643abd50917ca84266d9e55827
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2016-02-02 15:35:52 +0000

    Using basis Kosta code to build up generic multiphase equilibrium

---
+++ PythonUG_Models/PhaseEquilibrium/Main_Thermod.py
@@ -0,0 +1,91 @@
+
+#!/usr/bin/env python
+
+import matplotlib.pyplot as bplot
+import numpy as np
+import math
+import ThermoTools as ThT
+import GibbsFunction as GibbsF
+
+
+''' This is the main program that aims to calculate the vapour-liquid equilibrium
+       composition of a mixture containing "NComp" components. The thermodynamic
+       formulation can be found in the documentation under the ../doc directory.
+'''
+
+
+# Reading Input data from external file:
+ThT.ReadSet_Global_Variables()
+
+''' Printing the data from the input file '''
+print 'Species: ', ThT.Species
+print 'Temp of the mixture (initial, final, increment):', ThT.T_System
+print 'Pres of the mixture (initial, final, increment):', ThT.P_System
+print 'Temperature:', ThT.T_Crit
+print 'Pressure:', ThT.P_Crit
+print 'Molar Mass:', ThT.MolarMass
+print 'Binary Parameter:', ThT.BinaryParameter
+
+
+#
+# This is a temporary hack for testing the code, as the Simulated
+#    Annealing is introduced, MFrac and PhaseFrac will be estimated 
+#    during the calculations.
+#
+# MFrac stores the normalised composition (i.e., mass fractions or mole fractions) as
+#    MFrac_{1}(Vap), MFrac_{2}(Vap), ..., MFrac_{NComp}(Vap), MFrac_{1}(Liq), 
+#        MFrac_{2}(Liq), ..., MFrac_{NComp}(Liq) 
+#
+# PhaseFrac contains the mole (or mass) fraction of the phase. Assuming that there 
+#    are only 2 phases in equilibrium, Liquid and Vapour, therefore:
+#           PhaseFrac[ 0 ] + PhaseFrac[ 1 ] = 1.0
+#
+
+MFrac = [ 0. for i in range( ThT.NComp * ThT.NPhase ) ]  
+MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; MFrac[ 2 ] = 0.20 ; MFrac[ 3 ] = 0.2 # Vapour phase
+MFrac[ 4 ] = 0.10; MFrac[ 5 ] = 0.10; MFrac[ 6 ] = 0.20 ; MFrac[ 7 ] = 0.6 # Liquid phase
+ThT.Sum2One( 'Mass Fraction (Vapour)', MFrac[ :4 ] )
+ThT.Sum2One( 'Mass Fraction (Liquid)', MFrac[ 4: ] )
+
+PhaseFrac =  [ 0. for i in range( ThT.NPhase ) ]
+PhaseFrac[ 0 ] = 0.40 ; PhaseFrac[ 1 ] = 0.60 
+ThT.Sum2One( 'Phase Fraction', PhaseFrac )
+
+
+
+''' Initialising Temperature and Pressure '''
+Temp = ThT.T_System[ 0 ]
+Press = ThT.P_System[ 0 ] 
+
+Gibbs_Free = GibbsF.Calc_Gibbs( Temp, Press, MFrac, PhaseFrac )
+
+print 'Gibbs Free Energy:', Gibbs_Free
+
+
+""" This Loop can be used later to obtain equilibrium composition in a 
+      a wide range of temperature and pressure
+
+# Loop for temperature:
+while Temp <= ThT.T_System[ 1 ]:
+
+    # Loop for pressure:
+    Press = ThT.P_System[ 0 ] # Initialising Pressure
+    while Press <= ThT.P_System[ 1 ]:
+
+       # Gibbs_Free = GibbsF.Gibbs_Function( Temp, Press, MFrac, PhaseFrac )
+        a = 1.
+        print Temp, Press
+
+
+
+ ###
+        Press = Press + ThT.P_System[ 2 ] # End of pressure loop
+
+
+###
+    Temp = Temp + ThT.T_System[ 2 ]+1.e-6 # End of temperature loop
+
+print 'Final Press:', Press
+
+"""
+
---
+++ PythonUG_Models/PhaseEquilibrium/ThermoTools.py
@@ -0,0 +1,183 @@
+
+#!/usr/bin/env python
+
+import numpy as np
+import math
+import sys
+
+# =================== ASSOCIATED/EXTERNAL FUNCTIONS ====================#
+
+
+###
+### FUNCTION: Global Variables
+###
+
+def ReadSet_Global_Variables(): # Read variables from a external file called 'input.dat'
+    import csv # Using csv (comma separated values) module
+ 
+    global Rconst, NComp, NPhase, T_System, P_System, T_Crit, P_Crit, MolarMass, Species, Accentric_Factor, \
+        Z_Feed, BinaryParameter
+
+    Rconst = 8.314 # Gas constant [J/(gmol.K)]
+
+    ''' The first line MUST contain the number of components that will help to build up
+        all numpy arrays thus, '''
+
+    Are_There_Components = False
+
+    ''' Open a file named 'input.dat' that contains all thermo-physical parameters for
+        the model '''
+    with open( 'input.dat', 'rt' ) as file:
+        reader = csv.reader( file, delimiter = ' ', skipinitialspace = True )
+        
+        for row in reader:
+            if row == [] or ListOfCommentsStrings( row ): # List of comment strings that can be used
+                Nothing_To_Be_Done = True
+
+            elif row[ 0 ] == 'Number_Components': # This MUST be the first variable declared in the input file
+                NComp = int( row[ 1 ] )
+                Are_There_Components = True
+
+            elif Are_There_Components:
+                if row[ 0 ] == 'Number_Phases': # Number of phases
+                    NPhase = int( row[ 1 ] )
+    #
+                elif row[ 0 ] == 'System_Temperature': # Temperature of the mixture (in K)
+                    T_System = Reading_MixtureConditions_Float( row ) 
+    #
+                elif row[ 0 ] == 'System_Pressure': # Pressure of the mixture (in bar)
+                    P_System = Reading_MixtureConditions_Float( row )
+    #
+                elif row[ 0 ] == 'Crit_Temp': # Critical temperature (in K)
+                    T_Crit = ReadingRows_Float( row )
+    #
+                elif row[ 0 ] == 'Crit_Pres': # Critical pressure (in Pa)
+                    P_Crit = ReadingRows_Float( row )
+    #
+                elif row[ 0 ] == 'Molar_Mass': # Molar mass (in g/gmol)
+                    MolarMass = ReadingRows_Float( row )
+    #
+                elif row[ 0 ] == 'Components':
+                    Species = ReadingRows_String( row )
+    #
+                elif row[ 0 ] == 'Accentric_Factor':
+                    Accentric_Factor = ReadingRows_Float( row )
+    #
+                elif row[ 0 ] == 'Feed_Composition':
+                    Z_Feed = ReadingRows_Float( row )
+                    if ( abs( math.fsum( Z_Feed ) - 1. ) >= 1.e-5 ):
+                        print 'Summation of Compositions is ', math.fsum( Z_Feed ), \
+                            ' and it should be 1.0'
+                        sys.exit()
+    #
+                elif row[ 0 ] == 'BinaryInteraction':
+                    BinaryParameter = ReadingBinaryParameters( reader )
+    #
+            else:
+                print 'Number_components was not defined in the FIRST line'
+                sys.exit()
+                
+
+# This function reads a row containing float elements
+def ReadingRows_Float( row ):
+    Array = np.arange( float( NComp ) )
+    for i in xrange( 0, NComp ):
+        Array[ i ] = row[ i + 1 ]
+    return Array
+
+# This function reads a row containing float elements
+#    Line should contain:
+#       <name>  <initial>  <final>  <increment>
+#    where <name> = System_Temperature or System_Pressure,
+#    <final> and <increment> are used if we want to loop over temperature and/or pressure
+#    if we dont want to loop over the range, just keep <increment> = 0.0
+#    Also <initial> < <final> 
+def Reading_MixtureConditions_Float( row ):
+    Array = np.arange( float( 3 ) )
+    for i in xrange( 0, 3 ):
+        Array[ i ] = row[ i + 1 ]
+
+    if Array[ 0 ] > Array[ 1 ] or Array[ 0 ] < Array[ 2 ] or Array[ 1 ] < Array[ 2 ]:
+        print 'There is something wrong with ', row[ 0 ], 'field value.'
+        sys.exit()
+        
+    return Array
+
+# This function reads a row containing characters elements
+def ReadingRows_String( row ):
+    Array = []
+    Array.extend( row[ 1 : NComp + 1 ] )
+    
+    return Array
+
+# This function populates the binary interaction parameter (Kij) tensor in which Kij = Kji 
+#    and Kij = 0 (for i=1,NComp and j=1,NComp). Kij is stored as an array Kij[ node ] with
+#                             node = i * NComp + j
+#    For 3 components:
+#           K11   K12   K13
+#           K21   K22   K23
+#           K31   K32   K33
+#    In this case, K11 = K22 = K33 = 0 and
+#                  K12 = K21, K13 = K31 and K23 = K32
+#
+def ReadingBinaryParameters( reader ):
+    Kij = [ 0. for i in range( NComp ** 2 ) ]
+    Array_temp = [] 
+
+    for line in reader: # Reading input file for Kij and allocating the data in a list
+        Array_temp.append(line)
+
+    for k in range( NumberOfCombinations( NComp, 2 ) ):
+        temp = Array_temp[ k ]
+        i = int( temp[ 0 ] ) ; j = int( temp[ 1 ] )
+        node = ( i - 1 ) * NComp + ( j - 1 )
+        Kij[ node ] = float( temp[ 2 ] ) # Populating the upper triangular region
+
+    for i in range( NComp ):
+        for j in range( NComp ):
+            node1 = i * NComp + j
+            if i > j:
+                node2 = j * NComp + i 
+                Kij[ node1 ] = Kij[ node2 ] # Now populating the lower triangular region
+
+    return Kij
+        
+
+# This function calculates the number of possible combinations. Given a sample of M items, 
+#    we want to know how many subsets of N tuples can be generated as,
+#              Number_of_Combination = (M!) / [(N!) * ((M-N)!) ]
+def NumberOfCombinations( M, N ):
+    Comb = math.factorial( M ) / ( math.factorial( N ) * math.factorial( M - N ) )
+    return Comb
+
+
+# This function assess if the summation of compositions (mole/mass fraction) is equal to one
+def ListOfCommentsStrings( row ):
+    list = []
+    IsItaComment = False
+    if (     row[ 0 ] == ''      or \
+             row[ 0 ] == ' '     or \
+             row[ 0 ] == '  '    or \
+             row[ 0 ] == '   '   or \
+             row[ 0 ] == '#'     or \
+             row[ 0 ] == '##'    or \
+             row[ 0 ] == '###'   or \
+             row[ 0 ] == '# '    or \
+             row[ 0 ] == '## '   or \
+             row[ 0 ] == '### '  or \
+             row[ 0 ] == '#  '   or \
+             row[ 0 ] == '##  '  or \
+             row[ 0 ] == '###  ' ):
+        IsItaComment = True
+
+    return IsItaComment
+    
+
+# This function checks if an normalised compositional array (e.g., mass, mole or volume fractions)
+#     sums up to one
+def Sum2One( ident, Array ):
+    if abs( math.fsum( Array ) - 1. ) >= 1.e-7:
+        print 'Array ', ident, ' is not normalised correctly:', math.fsum( Array )
+        sys.exit()
+
+    return
