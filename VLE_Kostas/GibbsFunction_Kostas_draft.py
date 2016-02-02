12387e2940ebc487e67bd506a9162a50ef7624f5
kchrst <r02kc14@abdn.ac.uk>
2016-02-02 14:04:13 +0000

    Update GibbsFunction_Kostas_draft.py

--- VLE_Kostas/GibbsFunction_Kostas_draft.py
+++ VLE_Kostas/GibbsFunction_Kostas_draft.py
@@ -14,31 +14,62 @@ print '-------------------------------------------------------------------------
 # we have already declare the type and number of species from the input.dat
 print 'The number of Species is: ', ThT.NComp , ' while the Species are : ', ThT.Species , ' and the number of phases are :', ThT.NPhase 
 print '---------------------------------------------------------------------------------------------------------------------------------'
-nc = ThT.NComp # the number of compounds/species that I will have to loop over
+
+# I can either use the nc = ThT.NComp = the number of compounds/species that I will have to loop over
 
 print
-print 'Below we retrieve the T_crit , P_Crit and the accentric factor for each of the components '
+print 'Below we retrieve the characteristics/ input data for each of the components '
 
-for i in range(nc):
-    print '====================================================================================================================================='
-    print '  for the  ', ThT.Species[i] 
-    print '  T_Crit = ', ThT.T_Crit[i]
-    print '  P_Crit = ', ThT.P_Crit[i] 
-    print '  wmega  = ', ThT.Accentric_Factor[i]  
-    print '  Kij    = ', ThT.BinaryParameter[i]
-    print '  xi     = ', ThT.MolarMass[i]
-    print '  the initial feed composition is = ', ThT.Z_Feed[i]
-    print ''
+
+a_sum = 0,;b_sum = 0. # the initial sum for am and bm based on the eq. 2.7
+
+aij = [0. for i in range(ThT.NComp**2) ] # the aij has the same dimension like the kij, a square matrix where the main diagonal is 0
+
+for i in range(ThT.NComp):
+    for j in range(ThT.NComp):
+        print '  ======================================================================================================================= '
+    	print '  for the  ', ThT.Species[i] 
+    	print '  T_Crit = ', ThT.T_Crit[i]
+    	print '  P_Crit = ', ThT.P_Crit[i]            
+    	print '  wmega  = ', ThT.Accentric_Factor[i],'                                 accentric factor '
+    	print '  Kij    = ', ThT.BinaryParameter[i],'                                  the binary parameter '
+    	print '  xi     = ', ThT.MolarMass[i],'                                       the molar mass '
+    	print '  zi     = ', ThT.Z_Feed[i], '          overall feed mass fraction of the component '
+    	print ''
+        
+        # using the word node, that defines the componenet ij at the square matrix aij that has the same dimension like the kij
+        node = i * ThT.NComp + j
+        if i == j: # this is for the main diagonal
+            aij[ node ] = PR.PREoS_Calc_a( i , ThT.T_System[ 0 ] )  # I am calling the EOS_PR
+            print 'kosta m@l@k@ 1 to i = j node einai,',  aij[ node ]  
+        else: # this is for the rest of the elements of the square matrix of the aij
+            aij[ node ] = math.sqrt( PR.PREoS_Calc_a( i , ThT.T_System[ 0 ] ) * PR.PREoS_Calc_a( j , ThT.T_System[ 0 ] ) ) * ( 1. - ThT.BinaryParameter[ node ] )
+            print 'kosta m@l@k@ 2 to i /= j node einai,', aij[ node ]
+       
+        a_sum = a_sum + aij[ node ] * ThT.NComp[ i ] * ThT.NComp[ j ]
+    b_sum = b_sum + PR.PREoS_Calc_b[ i ] * ThT.NComp[ i ]
+
+print
+print 'the a_mixture = ', a_sum
+print 'the b_mixture = ', b_sum
+print
+        
+
 
 # calculating the fugacity coef. for each component at each phase greek phi[i] = fi / Pxi, xi = molar fraction or ThT.MolarMass
 
 # at this point i have to calculate the ai and bi 
+
+
+
+
+
 #                                       am and bm
 #                                  then  A and B
 #                                  then the cubic polynomial as below!
 
 
-#==================================    EOS - PR   ==================================================
+'''#==================================    EOS - PR   ==================================================
     def PREoS_Calc_a( i, T ): 
         k = 0.37464 + 1.5422 * ThT.Accentric_Factor[i] - 0.26992 * ThT.Accentric_Factor[i]**2
         alpha = ( 1. + k * ( 1. - math.sqrt( T / ThT.T_Crit[i] ) )) **2
@@ -63,7 +94,7 @@ for i in range(nc):
         coeffs[ 2 ] = Big_A - 2. * Big_B - 3. * Big_B**2
         coeffs[ 3 ] = - ( Big_A * Big_B - Big_B**3 - Big_B**2 )
         Z_root = np.roots( coeffs ) # Calculating the roots of the cubic eqn
-        print Z_root
+        print Z_root '''
 
 
 
@@ -76,4 +107,10 @@ def Calc_Gibbs( Temp, Press, MFrac, PhaseFrac ):
  
     return GibbsEnergy
 
+print
+print 'kosta m@l@k@ as long as you see that the script goes through the lines!'
+
+
+
 ''' R, Tc, Pc, w all from the input.dat '''
+
