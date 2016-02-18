216fcae2e20ebd90cdac3065025cfdc80044a3b1
kchrst <r02kc14@abdn.ac.uk>
2016-02-18 10:51:32 +0000

    Update Wilson_eq_test.py

--- VLE_Kostas/Wilson_eq_test.py
+++ VLE_Kostas/Wilson_eq_test.py
@@ -15,9 +15,6 @@ import EOS_PR as PR
 import time
 
 
-
-
-
 #MFrac = [ 0. for i in range( ThT.NComp * ThT.NPhase ) ]
 MFrac = [ 0. for i in range( 4 ) ]
 print 'the molar fraction is', MFrac
@@ -25,19 +22,33 @@ print 'the molar fraction is', MFrac
 MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; MFrac[ 2 ] = 0.20 ; MFrac[ 3 ] = 0.2 # Vapour phase
 #MFrac[ 4 ] = 0.10; MFrac[ 5 ] = 0.10; MFrac[ 6 ] = 0.20 ; MFrac[ 7 ] = 0.6 # Liquid phase
 
+
+Aij = [0. for i in range(4**2) ]
 Lamdaij = [0. for i in range(4**2) ]
 
-sum_denominator = 0
+sum2 = 0
+sum3_1 = 0
+sum3_2 = 0
 
-for i in range( 4 ):
-    for j in range( 4 ): 
-        print
-        print ' you are a the ', i,' - ', j, ' elemenent' 
-        node = i * 4 + j
-        if i == j:
-           Lamdaij[ node ] = 1; print ' the Greek Lamda is = ', Lamdaij[ node ]
-        else:
-           Lamdaij[ node ] = ( V2 / V1 )  * math.exp( ( - A[ node ] ) / R * T ); print ' the Greek Lamda is = ', Lamdaij[ node ] 
-        #print ' the molar farction of component ', i, ' is ', MFrac(i) 
-        sum_denominator = sum_denominator + MFrac(i)  
+for k in range( 4 ):
+    for i in range( 4 ):
+        for j in range( 4 ):
+            print '  - - - - - - - ' 
+            print ' for the activiti coef. of component ', k, ' in relation with the components ', i , j    
+            node = i * 3 + j
+            if i == j:
+               Lamdaij[ node ] = 1
+               print ' at the main diagonal, the Greek Lamda is = ', Lamdaij[ node ]
+               sum3_1 = sum3_1 + MFrac[ i ] * Lamdaij[ node ]
+            else:
+               Lamdaij[ node ] = 0.5                                 #( V2 / V1 )  * math.exp( ( - Aij[ node ] ) / R * T ); 
+               print ' the Greek Lamda is = ', Lamdaij[ node ]
+               sum3_2 = sum3_2 + MFrac[ i ] * Lamdaij[ node ]
+               
+            print ' the sum3_1 = ', sum3_1, 'the sum3_2 = ', sum3_2, ' while the final sum3 = ', sum3_1 + sum3_2
+            print
+
+
+
+
         
