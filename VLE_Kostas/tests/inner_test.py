09264e1b604aa499033667d55b61235fcd60cdcf
kchrst <r02kc14@abdn.ac.uk>
2016-02-29 09:16:30 +0000

    Create inner_test.py

---
+++ VLE_Kostas/tests/inner_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+import sys
+import thermotools_test as ThT
+import EOS_PR_test as PR
+import time
+
+x = [0.5, 1.]
+l = [2, 4]
+
+for i in range(2):
+    for j in range(2):
+        node = i * 2 + j
+        inner = 1 - np.log( np.dot( x, l ) ) - 1 
+        print 'at the node = ', node, '  the inner = ', inner
