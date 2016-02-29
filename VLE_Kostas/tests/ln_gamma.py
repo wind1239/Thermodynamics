8614bd3369defb2e64760947eba93fb6591660bf
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2016-02-29 14:24:37 +0000

    update

--- VLE_Kostas/tests/ln_gamma.py
+++ VLE_Kostas/tests/ln_gamma.py
@@ -5,32 +5,46 @@ import thermotools_test as ThT
 import EOS_PR_test as PR
 import time
 
-nc = 2
+nc = 3
 
-x = [0.5, 1.]
-l = [2, 4]
+x = [0.2, 0.6, 0.2]
+l = [1., 4.5, 3.8, 0.5, 1., 2.5, 8.6, 0.25, 1. ]
 suma = 0
 sumc = 0
 sumc_nom = 0
 sumc_denom = 0
 ln_gamma = 0
 
-for iphase in range(2):
+for iphase in range(1):
     print ' ---------------------------------------------------------- '
     print ' iphase = ', iphase
     for k in range(nc):
+        suma = 0
         for i in range(nc):
+            node = k * nc + i
+            print 'k, i, node:', k, i, node, x[i],l[node]
             print
-            suma = suma + x[i] * l[k]
-            print '                                the suma = ', suma
-            for j in range(nc):
-                print '                                  the xj = ' , x[j]
-                sumc_nom = sumc_nom + x[j] * l[i]
-                print ' at the component = ', j, '     the sumc_nom = ', sumc_nom  
-                for i in range(nc):
-                    sumc_denom = sumc_denom + x[i] * l[j]
+            suma = suma + x[i] * l[node]
+        print '                                the suma = ', suma
+        print
+# this is the second loop for the sumc term
+        for j in range(nc):
+            #print '                                  the xj = ' , x[j]
+            node1 = k *nc + j 
+            for i in range(nc):
+                node2 = i * nc + j
+                sumc_denom = sumc_denom + x[i] * l[node2]
+            sumc = sumc + x[j] * l[node1] / sumc_denom
+            #sumc = sumc_nom / sumc_denom
+            print 'k, j, node:', k, j, node, x[j],l[node1]
+        print '                            the sumc = ', sumc
+        ln_gamma = 1 - np.log(suma) - sumc
+        print '                        the ln_gamma = ', ln_gamma 
+        '''for i in range(nc):
+            sumc_denom = sumc_denom + x[i] * l[j]
                     print ' at the component = ',i , j, ' the sumc_denom = ', sumc_denom 
             sumc = sumc_nom / sumc_denom
             print '                          the final sumc = ', sumc
             ln_gamma == 1 - np.log(suma) - sumc    
-        print ' for iphase = ', iphase, ' the ln_gamma = ', ln_gamma 
+        print ' for iphase = ', iphase, ' the ln_gamma = ', ln_gamma '''
+
