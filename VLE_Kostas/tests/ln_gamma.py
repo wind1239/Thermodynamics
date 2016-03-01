c6052c9aef3720adc6f3e88a562700e27e911e14
kchrst <r02kc14@abdn.ac.uk>
2016-03-01 23:05:48 +0000

    Update ln_gamma.py

--- VLE_Kostas/tests/ln_gamma.py
+++ VLE_Kostas/tests/ln_gamma.py
@@ -8,14 +8,15 @@ import time
 nc = 3
 
 x = [0.2, 0.6, 0.2]
-l = [1., 4.5, 3.8, 0.5, 1., 2.5, 8.6, 0.25, 1. ]
+l = [1., 4.5, 3.8, 0.5, 1., 2.5, 8.6, 2.5, 1. ]
 suma = 0
 sumc = 0
 sumc_nom = 0
 sumc_denom = 0
 ln_gamma = 0
 
-'''for iphase in range(1):
+
+for iphase in range(1):
     print ' ---------------------------------------------------------- '
     print '                       iphase = ', iphase
     print ' ---------------------------------------------------------- '
@@ -26,9 +27,9 @@ ln_gamma = 0
             node = k * nc + i
             print ' k, i, node:', k, i, node, x[i],l[node]
             suma = suma + x[i] * l[node]
-        print '                              the ln( suma ) = ', np.log(suma) 
+        print '                              the ln(suma) = ', np.log(suma) 
         print
-        
+        '''
         # this is the second loop for the sumc term
         for j in range(nc):
             node1 = k *nc + j
@@ -46,14 +47,15 @@ ln_gamma = 0
 
 
 for k in range(nc):
-    #sumc_denom = 0
     for j in range(nc):
-        node1 = k *nc + j
+        node1 = j *nc + k 
+        sumc_nom = x[j] * l[node1]
+        #print ' the sumc_nom = ', sumc_nom, j, k
+        sum_denomi = 0
         for i in range(nc):
             node2 = i * nc + j
-            sum_denomi =  x[i] * l[node1]   
-            #sumc_denom = sumc_denom + x[i] * l[node2]
-        print ' the sum_denomi = ', sum_denomi, ' the sumc_denom = ', sumc_denom
+            sum_denomi =  sum_denomi + x[i] * l[node2]   
+            print ' the sum_denomi = ', sum_denomi, j, i
+            sumc_denom = sum_denomi
+        print '                             the sum_denom = ', sumc_denom
         #sumc = sumc + x[j] * l[node1] / sumc_denom 
-
-        
