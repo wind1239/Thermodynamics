a5c76d3c84f80904be676119159f3f84bd162616
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2016-03-11 17:50:27 +0000

    Updates

--- PythonUG_Models/PhaseEquilibrium/GibbsFunction.py
+++ PythonUG_Models/PhaseEquilibrium/GibbsFunction.py
@@ -5,15 +5,30 @@ import numpy as np
 import math
 import sys
 import ThermoTools as ThT
-import EoS as EoS
+import PhaseStability as Michaelsen
 
 
-def GibbsPhase( Temp, Press, Comp, iphase ):
+def GibbsObjectiveFunction( InitialAssessment, Temp, Press, Comp_Phase ):
+
+    if ThT.NPhase > 2:
+        print 'This function was hacked to work only on a 2-phases system, it thus needs to be generalised.'
+        sys.exit()
+
+    Alpha = Michaelsen.AlphaPhases() # Phase distribution
+    if InitialAssessment:
+        if Alpha_Phase[ 1 ] > Alpha_Phase[ 0 ]:
+            iphase = 0 # Vapour
+        else:
+            iphase = 1 # Liquid
+
+        GibbsFunct = Calculating_Gibbs( Temp, Press, Comp_Phase, iphase )
+        
+
 
     
     
 
-    return GibbsEnergy
+    return GibbsMolar
     
 
 
