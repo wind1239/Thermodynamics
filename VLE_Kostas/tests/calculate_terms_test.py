d2c99f3352497d8be6f5a5837a92b20058317a0b
kchrst <r02kc14@abdn.ac.uk>
2016-03-14 15:46:40 +0000

    Update calculate_terms_test.py

--- VLE_Kostas/tests/calculate_terms_test.py
+++ VLE_Kostas/tests/calculate_terms_test.py
@@ -8,54 +8,50 @@ import pylab
 import time
 
 
+def Q( MFrac ):
+    Q1 = [ 0. for i in range( ThT.NComp ) ]
+    for i in range(ThT.NComp):
+        for j in range(ThT.NComp):
+            Q1[ i ] = Q1[ i ] + MFrac[ i ] * MFrac[ j ] * ( PR.PREoS_Calc_b( i ) - PR.PREoS_Calc_a( i, ThT.T_Crit[i] ) / ThT.Rconst * ThT.T_System[ 0 ] )
+    print
+    return Q1
+
+def D( Mfrac ):
+    c = (1 / np.sqrt(2)) * np.log( np.sqrt(2)-1 )
+    D1 = [ 0. for i in range( ThT.NComp ) ]
+    for i in range(ThT.NComp):
+        D1 = D1 + MFrac[ i ] * ( PR.PREoS_Calc_b( i ) - PR.PREoS_Calc_a( i, ThT.T_Crit[i] ) / ThT.Rconst * ThT.T_System[ 0 ] ) + ( lng.gibbs( MFrac ) / c * ThT.Rconst * ThT.T_System[ 0 ] )
+    print
+    return D1
+
+
+
+# = = = = = = = = = # = = = = = = = = = # = = = = = = = = = # = = = = = = = = = #
 Rconst = 8.314 # Gas constant [J/(gmol.K)]
 
 ThT.ReadSet_Global_Variables()
 
 MFrac = [ 0. for i in range( ThT.NComp ) ]
-print '  the initial molar fraction before reading from the input.dat is', MFrac
+print '  the initial molar fraction before reading from the input.dat is ', MFrac
 # declare a vector with MFrac values - molar fraction
 MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; # Vapour phase
 #MFrac[ 2 ] = 0.10; MFrac[ 3 ] = 0.10; # Liquid phase
 
+q = [ 0. for i in range( ThT.NComp ) ]
+q = Q( MFrac )
+print '  Q = ', q
+print
 
-
-bart = [0. for i in range(ThT.NComp**2) ]
-bm = 0
-am = 0
-
-bm_nom = 0
-bm_denom = 0
-for i in range(ThT.NComp):
-    for j in range(ThT.NComp):
-        node = i * ThT.NComp + j
-        k = 0.37464 + 1.5422 * ThT.Accentric_Factor[i] - 0.26992 * ThT.Accentric_Factor[i]**2
-        alpha = ( 1. + k * ( 1. - math.sqrt( ThT.T_System[ 0 ] / ThT.T_Crit[i] ) )) **2
-        a_i = 0.45724 * ( ThT.Rconst * ThT.T_Crit[i] )**2 / ThT.P_Crit[i] * alpha
-        b_i = 0.07780 * ThT.Rconst * ThT.T_Crit[i]  / ThT.P_Crit[i]
-        bart[ node ] = ( b_i - ( a_i / ThT.Rconst * ThT.T_Crit[i]) ) + ( b_i - ( a_i / ThT.Rconst * ThT.T_Crit[i]) ) * ( 1 - ThT.BinaryParameter[node] ) / 2
-        bm_nominator = MFrac[ i ] * MFrac[ j ] * ( bart[ node ] ) 
-        bm_denominator =  1 - 1 / ( ThT.Rconst * ThT.T_Crit[i] ) - lng.gibbs( MFrac ) + np.inner( MFrac , ( a_i/b_i ) )
-        bm = bm + bm_nominator / bm_denominator
-        am = bm * ( bm_denominator )
-        print ' ----------- the component ', ThT.Species[i],' with respect to' , ThT.Species[j], '---------------- '    
-        print '  the k = ', k,'  the alphai = ', alpha, '  the ai = ', a_i, '  the bi = ', b_i  
-        print '  the bart = ', bart[ node ], ' the bm = ', bm, ' the am = ', am
-        print 
-    print 
-         
-
-def Q(MFrac):
-    Q = bm_nominator
-    print ' Q = ', Q
-    return Q
-
-def D(Mfrac):
-    D = bm_denominator
-    print ' D = ', D
-    return D
+d = [ 0. for i in range( ThT.NComp ) ]
+d = D( MFrac )
+print '  D = ', d
+print
 
 c = (1 / np.sqrt(2)) * np.log( np.sqrt(2)-1 )                    # c term from the eq. 2.26
-print '  the c = ', c
+print '  c = ', c
 print
+
+
+
+
         
