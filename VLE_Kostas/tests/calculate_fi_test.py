110055f60f7dcb6c4ab1f060c79c89dffdc15610
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2016-03-16 17:39:30 +0000

    update

--- VLE_Kostas/tests/calculate_fi_test.py
+++ VLE_Kostas/tests/calculate_fi_test.py
@@ -10,6 +10,35 @@ import EOS_PR_test as PR
 import ln_gamma_test as lng
 import calculate_terms_test as terms
 
+
+def calc_B( MFrac ):
+    b = terms.B( MFrac ) 
+    fi = b + 1 
+    return fi
+
+
+
+ThT.ReadSet_Global_Variables()
+
+MFrac = [ 0. for i in range( ThT.NComp ) ]
+print '  the initial molar fraction before reading from the input.dat is ', MFrac
+# declare a vector with MFrac values - molar fraction
+MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; # Vapour phase
+#MFrac[ 2 ] = 0.10; MFrac[ 3 ] = 0.10; # Liquid phase
+
+
+zmax , zmin = PR.Cubic_PR( ThT.T_System[0], ThT.P_System[0], terms.AM( MFrac ), terms.BM( MFrac ) )
+y = zmax
+x = zmin
+print ' zmax root for the vapour phase = ', y
+print ' zmin root for the liquid phase = ', x
+print
+
+
+phi = calc_B( MFrac )
+print ' fi = ', phi 
+
+
 '''e = lngamma.kostas( 2, 2, 2, 2)
 print ' the e = ', e'''
 
@@ -25,27 +54,21 @@ def changeme( mylist ):
 mylist = [10,20,30];
 changeme( mylist );
 print "Values outside the function: ", mylist
-'''
+
 
 MFrac = 1
 
 partial1 = 1 / 1 - terms.D( MFrac )
 print ' partial 1 = ', partial1
+'''
+
+#MFrac = [ 0 , 0 ]
 
 
 
 
-MFrac = [ 0 , 0 ]
 
 
-def calc_B( MFrac ):
-    b = terms.B( MFrac )
-    fi = b
-    return fi
-
-phi = calc_B( MFrac )
-
-print ' fi = ', phi 
 
 
 
