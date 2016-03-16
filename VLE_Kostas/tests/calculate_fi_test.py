94b329bc695d1e6f7c01bca80206ffc116768b8b
kchrst <r02kc14@abdn.ac.uk>
2016-03-16 15:48:41 +0000

    Update calculate_fi_test.py

--- VLE_Kostas/tests/calculate_fi_test.py
+++ VLE_Kostas/tests/calculate_fi_test.py
@@ -27,11 +27,63 @@ print ' zmax root for the vapour phase = ', y
 print ' zmin root for the liquid phase = ', x
 
 
-
 def calc_chempot( iphase, NComp ):
     print ' am = ', terms.AM 
     
     return chempoti
+
+#######################################################################################################
+#!/usr/bin/env python
+
+# a function that contains the variables to calculate the greek_fi
+
+import numpy as np
+import math
+import sys
+import thermotools_test as ThT
+import EOS_PR_test as PR
+import ln_gamma_test as lng
+import calculate_terms_test as terms
+
+'''e = lngamma.kostas( 2, 2, 2, 2)
+print ' the e = ', e'''
+
+'''
+# Function definition is here
+def changeme( mylist ):
+   "This changes a passed list into this function"
+   mylist.append([1,2,3,4]);
+   print "Values inside the function: ", mylist
+   return
+
+# Now you can call changeme function
+mylist = [10,20,30];
+changeme( mylist );
+print "Values outside the function: ", mylist
+'''
+
+
+
+MFrac = [ 0 , 0 ]
+
+
+def calc_B( MFrac ):
+    b = terms.B( MFrac )
+    fi = b
+    return fi
+
+phi = calc_B( MFrac )
+
+print ' fi = ', phi 
+
+
+
+
+    
+
+
+
+
 
 
 # = = = = = = = = = # = = = = = = = = = # = = = = = = = = = # = = = = = = = = = #
@@ -45,6 +97,6 @@ print '  the initial molar fraction before reading from the input.dat is ', MFra
 # declare a vector with MFrac values - molar fraction
 MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; # Vapour phase
 #MFrac[ 2 ] = 0.10; MFrac[ 3 ] = 0.10; # Liquid phase
-
+#######################################################################################################
 
     
