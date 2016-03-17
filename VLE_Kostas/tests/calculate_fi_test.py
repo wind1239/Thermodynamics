43988897e0b6f060389d9a82691d7618351a1feb
kchrst <r02kc14@abdn.ac.uk>
2016-03-17 12:32:30 +0000

    Update calculate_fi_test.py

--- VLE_Kostas/tests/calculate_fi_test.py
+++ VLE_Kostas/tests/calculate_fi_test.py
@@ -11,10 +11,30 @@ import ln_gamma_test as lng
 import calculate_terms_test as terms
 
 
-def calc_B( MFrac ):
-    b = terms.B( MFrac ) 
-    fi = b + 1 
+def CALC_FI( MFrac ):
+    #z = zmax
+    q = 1       #( 1 / ( 1 - terms.D( MFrac ) ) ) * terms.DQ( MFrac ) - terms.Q( MFrac ) / ( 1 - terms.D( MFrac ) **2  ) * ( 1 - terms.DD( MFrac ) )
+    d =  terms.D( MFrac ) * q + terms.BM( MFrac ) * ( 1 - terms.DD( MFrac ) )
+    term1 = - np.log( zmax - terms.B( MFrac ) ) 
+    print ' term 1 = ', term1, zmax
+    #term2 = ( 1 / terms.BM(MFrac) ) * q * ( z - 1 )
+    #print ' term 2 =', term2
+    #term3 = ( 0.5 / np.sqrt(2) ) * ( ( 1/ terms.AM( MFrac ) ) * d * ThT.Rconst * ThT.T_System[ 0 ] ) - ( ( 1 / terms.BM( MFrac ) ) * q ) 
+    #print ' term 3 = ', term3 
+    #term4 = np.log( ( z / terms.B( MFrac )  + 1 - np.sqrt(2) ) / ( z / terms.B( MFrac )  + 1 + np.sqrt(2) ) )
+    #print ' term4 = ', term4
+    fi = term1 #+ term2 + term3 * term4
     return fi
+
+def CALC_CHEMPOT( MFrac ):
+    for iphase in range( ThT.NPhase ): 
+        for i in range( iphase * ThT.NComp , iphase * ThT.NComp + ThT.NComp )  :
+            print ' iphase = ', iphase, ' i = ', i   
+            chempot =  ThT.Rconst * ThT.T_System[ 0 ] * CALC_FI( MFrac ) + np.log( ThT.P_System[ 0 ] * MFrac[ i ])
+            print ' iphase = ', iphase, ' i = ', i,' the chempot = ', chempot
+            print
+    return chempot
+
 
 
 
@@ -26,7 +46,6 @@ print '  the initial molar fraction before reading from the input.dat is ', MFra
 MFrac[ 0 ] = 0.40; MFrac[ 1 ] = 0.20; # Vapour phase
 #MFrac[ 2 ] = 0.10; MFrac[ 3 ] = 0.10; # Liquid phase
 
-
 zmax , zmin = PR.Cubic_PR( ThT.T_System[0], ThT.P_System[0], terms.AM( MFrac ), terms.BM( MFrac ) )
 y = zmax
 x = zmin
@@ -34,41 +53,16 @@ print ' zmax root for the vapour phase = ', y
 print ' zmin root for the liquid phase = ', x
 print
 
-
-phi = calc_B( MFrac )
+phi = CALC_FI( MFrac )
 print ' fi = ', phi 
 
-
-'''e = lngamma.kostas( 2, 2, 2, 2)
-print ' the e = ', e'''
-
-'''
-# Function definition is here
-def changeme( mylist ):
-   "This changes a passed list into this function"
-   mylist.append([1,2,3,4]);
-   print "Values inside the function: ", mylist
-   return
-
-# Now you can call changeme function
-mylist = [10,20,30];
-changeme( mylist );
-print "Values outside the function: ", mylist
-
-
-MFrac = 1
-
-partial1 = 1 / 1 - terms.D( MFrac )
-print ' partial 1 = ', partial1
-'''
-
-#MFrac = [ 0 , 0 ]
+chempot_mi = CALC_CHEMPOT( MFrac )
+print ' greek_mi = ', chempot_mi
 
 
 
 
-
-
+ 
 
 
 
