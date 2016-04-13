afe387ef480ac00ab6a4e658839a514528c60dc7
kchrst <r02kc14@abdn.ac.uk>
2016-04-13 22:40:46 +0100

    Update calculate_chemical_potential_test.py

--- VLE_Kostas/test_running/calculate_chemical_potential_test.py
+++ VLE_Kostas/test_running/calculate_chemical_potential_test.py
@@ -7,13 +7,15 @@ import thermotools_test as ThT
 import calculate_fi_test as fi
 
 def Calc_ChemPot( iphase, MFrac ):
-    print ' i am finally in the chemical potetnial function '
+    print ' * i am finally in the chemical potetnial function '
 
     ChemPot = [0. for i in range(ThT.NComp) ] 
-    lnPXi = [0. for i in range(ThT.NComp) ] 
-
+    lnPXi = [0. for i in range(ThT.NComp) ]
+    print 
+    print ' ---------------------------------------------------------------------------- lnPXi =',lnPXi, ' ChemPot =', ChemPot 
+    print
     lnphi = fi.CALC_FI( iphase, MFrac ) # lnphi is the 1st term of the rhs of the equation and contains Nc components 
-
+    print ' --------------------------------------------------------------------- the lnphi from above is = ', lnphi 
     # Calculating second-term of the rhs
     for icomp in range( ThT.NComp ):
          lnPXi[ icomp ] = math.log( ThT.P_System[ 0 ] * MFrac[ icomp ] )
