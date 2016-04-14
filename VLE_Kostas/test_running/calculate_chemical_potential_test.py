99168f7876cf474e5d7b33c6298c185e61849120
kchrst <r02kc14@abdn.ac.uk>
2016-04-14 14:06:54 +0100

    Update calculate_chemical_potential_test.py

--- VLE_Kostas/test_running/calculate_chemical_potential_test.py
+++ VLE_Kostas/test_running/calculate_chemical_potential_test.py
@@ -11,16 +11,16 @@ def Calc_ChemPot( iphase, MFrac ):
 
     ChemPot = [0. for i in range(ThT.NComp) ] 
     lnPXi = [0. for i in range(ThT.NComp) ]
-    print 
-    print ' ---------------------------------------------------------------------------- lnPXi =',lnPXi, ' ChemPot =', ChemPot 
+    #print 
+    #print ' ---------------------------------------------------------------------------- lnPXi =',lnPXi, ' ChemPot =', ChemPot 
     print
     lnphi = fi.CALC_FI( iphase, MFrac ) # lnphi is the 1st term of the rhs of the equation and contains Nc components 
-    print ' --------------------------------------------------------------------- the lnphi from above is = ', lnphi 
+    #print ' --------------------------------------------------------------------- the lnphi from above is = ', lnphi 
     # Calculating second-term of the rhs
     for icomp in range( ThT.NComp ):
          lnPXi[ icomp ] = math.log( ThT.P_System[ 0 ] * MFrac[ icomp ] )
          ChemPot[ icomp ] = ThT.Rconst * ThT.T_System[0] * ( lnphi[ icomp ] - lnPXi[ icomp ] )
-         print ' the chempot = ', ChemPot[ icomp ]
+         print '  I am in the chempot function --- the chempot = ', ChemPot[ icomp ]
     return ChemPot
 
 '''
