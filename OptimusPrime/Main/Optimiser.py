563e8a0297304fd05c8c7b7c8763a302936a6bba
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2018-03-02 14:53:31 +0000

    updates

--- OptimusPrime/Main/Optimiser.py
+++ OptimusPrime/Main/Optimiser.py
@@ -1,15 +1,43 @@
-
 #!/usr/bin/env python
-
 import sys, os
 import time
 import SystemPaths as SyP
-lib_path = os.path.abspath('../SAA' ) # path to SAA
+#import InitCalls as IC
+
+# Initialisation of Environmental Variables 
+SyP.EnvirVar()
+
+# Input of argument:
+if len( sys.argv ) < 3:
+    Print.HelpInput()
+    sys.exit()
+    
+else:
+    Method = sys.argv[ 1 ] ; Task = sys.argv[ 2 ]
+
+    if Task == 'Problem' or Task == 'Problems':
+        if len( sys.argv ) == 4: # Dealing with a thermodynamic problem
+            ProblemFileName = sys.argv[ 3 ] ; PhaseEquilibria = sys.argv[ 3 ] 
+        else: # dealing with any other optimisation problem
+            ProblemFileName = sys.argv[ 3 ]
+        
+    elif Task == 'Benchmarks':
+        TestCases = sys.argv[ 3 ]
+        
+# Creating Global Variables for directories pathways
+if Task == 'Benchmarks':
+    SyP.PathWays( Task, Method )
+else:
+    SyP.PathWays( Task, Method, Thermodynamics = PhaseEquilibria )
+
+lib_path = os.path.abspath(SyP.OptimiserPATH) # path to SAA
 sys.path.append( lib_path ) # <== Adding the above in the sys path for python
-import SA_IO as IO
-import Simulated_Annealing as ASA
-import SA_Print as Print
 import SAA_Tools as SaT
+import InitCalls as IC
+SaT.Time_Init = time.clock()
+
+# Starting Code
+IC.MainCode()
 
 
 ###                                     ###
@@ -49,62 +77,3 @@ import SAA_Tools as SaT
                 ii) "Test_A.phys": contains information -re the physical problem.
                iii) "Test_A.py": contains the main call for the test-function.
                                                                                            """
-
-#global Time_Init, Time_Final
-SyP.EnvirVar()
-
-# Measuring CPU-time in the beginning of the simulation:
-SaT.Time_Init = time.clock()
-
-# Input of argument:
-if len( sys.argv ) < 3:
-    Print.HelpInput()
-    sys.exit()
-    
-else:
-    Method = sys.argv[ 1 ] ; Task = sys.argv[ 2 ]
-
-    if Task == 'Problem' or Task == 'Problems':
-        if len( sys.argv ) == 5: # Dealing with a thermodynamic problem
-            PhaseEquilibria = sys.argv[ 3 ] ; ProblemFileName = sys.argv[ 4 ]
-        else: # dealing with any other optimisation problem
-            ProblemFileName = sys.argv[ 3 ]
-        
-    elif Task == 'Benchmarks':
-        TestCases = sys.argv[ 3 ]
-        
-# Creating Global Variables for directories pathways
-SyP.PathWays( Task, Method, Thermodynamics = PhaseEquilibria )
-
-
-
-""" Creating a file for general output """
-if Task == 'Problem' or Task == 'Problems':
-    IO.OutPut( Task, Method, Problem_Name = ProblemFileName )
-else:
-    IO.OutPut( Task, Method, Benchmark_Number = TestCases )
-
-""" Calling the optimisation routine """
-
-if ( Method == 'SAA' or Method == 'SA' ):
-    if Task == 'Problem' or Task == 'Problems':
-        X_Opt, F_Opt = ASA.SimulatedAnnealing( Method, Task, FileName = ProblemFileName, Thermodynamics = PhaseEquilibria )
-    elif Task == 'Benchmarks':
-        X_Opt, F_Opt = ASA.SimulatedAnnealing( Method, Task, FileName = TestCases )
-        
-else:
-    print "Method", Method, "has not been defined yet!"
-    sys.exit()
-
-    
-
-# Measuring CPU-time at the end of the simulation and print it out:
-SaT.Time_Final = time.clock()
-
-print 'Total CPU simulation time:', SaT.Time_Final - SaT.Time_Init
-IO.f_SAOutput.write(  '\n' '\n' )
-IO.f_SAOutput.write( '============================================================ \n' )
-IO.f_SAOutput.write( '  TOTAL CPU TIME FOR THE OPTIMISATION PROBLEM                \n' )
-IO.f_SAOutput.write( '============================================================ \n' )
-IO.f_SAOutput.write( '\n' )
-IO.f_SAOutput.write( 'CPU Time: {a:}'.format( a = SaT.Time_Final - SaT.Time_Init ) + '\n' )
