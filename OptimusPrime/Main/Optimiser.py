e9b1d5c4be850a344e290b7f634a24a21d1de491
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2018-03-08 15:17:13 +0000

    updates

--- OptimusPrime/Main/Optimiser.py
+++ OptimusPrime/Main/Optimiser.py
@@ -1,44 +1,9 @@
+
 #!/usr/bin/env python
+
 import sys, os
 import time
 import SystemPaths as SyP
-#import InitCalls as IC
-
-# Initialisation of Environmental Variables 
-SyP.EnvirVar()
-
-# Input of argument:
-if len( sys.argv ) < 3:
-    Print.HelpInput()
-    sys.exit()
-    
-else:
-    Method = sys.argv[ 1 ] ; Task = sys.argv[ 2 ]
-
-    if Task == 'Problem' or Task == 'Problems':
-        if len( sys.argv ) == 4: # Dealing with a thermodynamic problem
-            ProblemFileName = sys.argv[ 3 ] ; PhaseEquilibria = sys.argv[ 3 ] 
-        else: # dealing with any other optimisation problem
-            ProblemFileName = sys.argv[ 3 ]
-        
-    elif Task == 'Benchmarks':
-        TestCases = sys.argv[ 3 ]
-        
-# Creating Global Variables for directories pathways
-if Task == 'Benchmarks':
-    SyP.PathWays( Task, Method )
-else:
-    SyP.PathWays( Task, Method, Thermodynamics = PhaseEquilibria )
-
-lib_path = os.path.abspath(SyP.OptimiserPATH) # path to SAA
-sys.path.append( lib_path ) # <== Adding the above in the sys path for python
-import SAA_Tools as SaT
-import InitCalls as IC
-SaT.Time_Init = time.clock()
-
-# Starting Code
-IC.MainCode()
-
 
 ###                                     ###
 ###              MAIN CODE              ###
@@ -77,3 +42,61 @@ IC.MainCode()
                 ii) "Test_A.phys": contains information -re the physical problem.
                iii) "Test_A.py": contains the main call for the test-function.
                                                                                            """
+# Input of argument:
+if len( sys.argv ) < 3:
+    Print.HelpInput()
+    sys.exit()
+    
+else:
+    Method = sys.argv[ 1 ] ; Task = sys.argv[ 2 ]
+
+    if Task == 'Problem' or Task == 'Problems':
+        if len( sys.argv ) == 5: # Dealing with a thermodynamic problem
+            PhaseEquilibria = sys.argv[ 3 ] ; ProblemFileName = sys.argv[ 4 ]
+            SyP.EnvirVar( Task, Method, Thermodynamics = PhaseEquilibria )# Creating Global Variables for directories pathways
+        else: # dealing with any other optimisation problem
+            ProblemFileName = sys.argv[ 3 ]
+            sys.exit('Need to be finished')
+        
+    elif Task == 'Benchmarks':
+        TestCases = sys.argv[ 3 ]
+        SyP.EnvirVar( Task, Method )# Creating Global Variables for directories pathways
+        
+import SA_IO as IO
+import Simulated_Annealing as ASA
+import SA_Print as Print
+import SAA_Tools as SaT
+
+# Measuring CPU-time in the beginning of the simulation:
+SaT.Time_Init = time.clock()
+
+""" Creating a file for general output """
+if Task == 'Problem' or Task == 'Problems':
+    IO.OutPut( Task, Method, Problem_Name = ProblemFileName )
+else:
+    IO.OutPut( Task, Method, Benchmark_Number = TestCases )
+
+""" Calling the optimisation routine """
+
+if ( Method == 'SAA' or Method == 'SA' ):
+    if Task == 'Problem' or Task == 'Problems':
+        X_Opt, F_Opt = ASA.SimulatedAnnealing( Method, Task, FileName = ProblemFileName, Thermodynamics = PhaseEquilibria )
+    elif Task == 'Benchmarks':
+        X_Opt, F_Opt = ASA.SimulatedAnnealing( Method, Task, FileName = TestCases )
+        
+else:
+    print "Method", Method, "has not been defined yet!"
+    sys.exit()
+
+    
+
+# Measuring CPU-time at the end of the simulation and print it out:
+SaT.Time_Final = time.clock()
+
+print 'Total CPU simulation time:', SaT.Time_Final - SaT.Time_Init
+IO.f_SAOutput.write(  '\n' '\n' )
+IO.f_SAOutput.write( '============================================================ \n' )
+IO.f_SAOutput.write( '  TOTAL CPU TIME FOR THE OPTIMISATION PROBLEM                \n' )
+IO.f_SAOutput.write( '============================================================ \n' )
+IO.f_SAOutput.write( '\n' )
+IO.f_SAOutput.write( 'CPU Time: {a:}'.format( a = SaT.Time_Final - SaT.Time_Init ) + '\n' )
--- OptimusPrime/Main/SystemPaths.py
+++ OptimusPrime/Main/SystemPaths.py
@@ -5,32 +5,20 @@ import sys, os
 
 """ This function checks if the environmental variable (EV) OptimusPATH has been set up.
        The EV OptimusPATH gives the address (i.e., pathway of the directory) of the OptimusPrime."""
-def EnvirVar():
-    if os.environ.get('OptimusPATH') is None:
-
-        msg = (
-            '===================================================================================================== \n'
-            '  The Environmental Variable (EV) *OptimusPATH* was not defined. This can be solved by either: \n '
-            '     a) Set up this EV permanently by adding the following line at the end of the .bashrc file (at the root directory): \n'
-            '            export OptimusPATH="Path_To_OptimusPrime" \n'
-            '        e.g., \n'
-            '            export OptimusPATH="/data2/UoA/ThermodynamicModels/OptimusPrime", or; \n'
-            '     b) Set up this EV temporarily by adding the following at the shell-erminal: \n'
-            '            export OptimusPATH="Path_To_OptimusPrime" \n'
-            '        e.g., \n'
-            '            export OptimusPATH="/data2/UoA/ThermodynamicModels/OptimusPrime"  \n'
-            ''
-            '===================================================================================================== \n')
-        print(msg)
-        sys.exit( '                              **OptimusPATH** not defined.\n' )
-
-""" This function defines the environmental paths for the main directories
-    used by the code. """
-def PathWays( Task, Method, **kwargs ):
+def EnvirVar( Task, Method, **kwargs ):
     global RootPATH, MainPATH, OptimiserPATH, SLVE_PATH, ProbPATH
     
-    RootPATH = os.environ.get('OptimusPATH')
-    MainPATH = RootPATH + '/Main/'
+    if os.environ.get('OptimusPATH') is None:
+        PATH = os.getcwd() ; StringMain = '/Main' ; PATH2 = PATH.strip( StringMain )
+        os.environ['OptimusPATH'] = PATH2 ; RootPATH = os.environ.get('OptimusPATH')
+        MainPATH = RootPATH + '/Main/'
+        #MainPATH = os.getcwd() ; 
+        #StringMain = '/Main' ; RootPATH = MainPATH.strip( StringMain )
+    else:
+        RootPATH = os.environ.get('OptimusPATH')
+        MainPATH = RootPATH + '/Main/'
+
+    sys.path.append( RootPATH ); sys.path.append( MainPATH )
 
     if Task == 'Problem' :
         ProbPATH = RootPATH + '/Tests/Problems/'
@@ -38,16 +26,23 @@ def PathWays( Task, Method, **kwargs ):
         ProbPATH = RootPATH + '/Tests/Benchmarks/'
     else:
         sys.exit('Non-existent option for PROBLEM type')
+    sys.path.append( ProbPATH )
 
     if Method == 'SAA' or Method == 'SA':
         OptimiserPATH = RootPATH + '/SAA/'
     else:
         sys.exit('Non-existent option for METHOD option')
+    sys.path.append( OptimiserPATH )
 
     if kwargs:
         for key in kwargs:
             if ( key == 'Thermodynamics' ):
                 SLVE_PATH = RootPATH + '/PhaseEquilibria/'
+                sys.path.append( SLVE_PATH )
+
+    print RootPATH, MainPATH, OptimiserPATH
+
+    return
 
 
 
