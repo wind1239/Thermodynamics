cb21533d4c332e36fb6a928299fa28662e40de81
Jeff Gomes <jefferson.gomes@abdn.ac.uk>
2018-03-09 13:11:21 +0000

    updates

--- OptimusPrime/Main/SystemPaths.py
+++ OptimusPrime/Main/SystemPaths.py
@@ -9,38 +9,46 @@ def EnvirVar( Task, Method, **kwargs ):
     global RootPATH, MainPATH, OptimiserPATH, SLVE_PATH, ProbPATH
     
     if os.environ.get('OptimusPATH') is None:
-        PATH = os.getcwd() ; StringMain = '/Main' ; PATH2 = PATH.strip( StringMain )
-        os.environ['OptimusPATH'] = PATH2 ; RootPATH = os.environ.get('OptimusPATH')
-        MainPATH = RootPATH + '/Main/'
-        #MainPATH = os.getcwd() ; 
-        #StringMain = '/Main' ; RootPATH = MainPATH.strip( StringMain )
+
+        msg = (
+            '===================================================================================================== \n'
+            '  The Environmental Variable (EV) *OptimusPATH* was not defined. This can be solved by either: \n '
+            '     a) Set up this EV permanently by adding the following line at the end of the .bashrc file (at the root directory): \n'
+            '            export OptimusPATH="Path_To_OptimusPrime" \n'
+            '        e.g., \n'
+            '            export OptimusPATH="/data2/UoA/ThermodynamicModels/OptimusPrime", or; \n'
+            '     b) Set up this EV temporarily by adding the following at the shell-erminal: \n'
+            '            export OptimusPATH="Path_To_OptimusPrime" \n'
+            '        e.g., \n'
+            '            export OptimusPATH="/data2/UoA/ThermodynamicModels/OptimusPrime"  \n'
+            ''
+            '===================================================================================================== \n')
+        print(msg)
+        sys.exit( '                              **OptimusPATH** not defined.\n' )
     else:
         RootPATH = os.environ.get('OptimusPATH')
         MainPATH = RootPATH + '/Main/'
+        sys.path.append( RootPATH ); sys.path.append( MainPATH )
 
-    sys.path.append( RootPATH ); sys.path.append( MainPATH )
+        if Task == 'Problem' :
+            ProbPATH = RootPATH + '/Tests/Problems/'
+        elif Task == 'Benchmarks' :
+            ProbPATH = RootPATH + '/Tests/Benchmarks/'
+        else:
+            sys.exit('Non-existent option for PROBLEM type')
+        sys.path.append( ProbPATH )
 
-    if Task == 'Problem' :
-        ProbPATH = RootPATH + '/Tests/Problems/'
-    elif Task == 'Benchmarks' :
-        ProbPATH = RootPATH + '/Tests/Benchmarks/'
-    else:
-        sys.exit('Non-existent option for PROBLEM type')
-    sys.path.append( ProbPATH )
+        if Method == 'SAA' or Method == 'SA':
+            OptimiserPATH = RootPATH + '/SAA/'
+        else:
+            sys.exit('Non-existent option for METHOD option')
+        sys.path.append( OptimiserPATH )
 
-    if Method == 'SAA' or Method == 'SA':
-        OptimiserPATH = RootPATH + '/SAA/'
-    else:
-        sys.exit('Non-existent option for METHOD option')
-    sys.path.append( OptimiserPATH )
-
-    if kwargs:
-        for key in kwargs:
-            if ( key == 'Thermodynamics' ):
-                SLVE_PATH = RootPATH + '/PhaseEquilibria/'
-                sys.path.append( SLVE_PATH )
-
-    print RootPATH, MainPATH, OptimiserPATH
+        if kwargs:
+            for key in kwargs:
+                if ( key == 'Thermodynamics' ):
+                    SLVE_PATH = RootPATH + '/PhaseEquilibria/'
+                    sys.path.append( SLVE_PATH )
 
     return
 
